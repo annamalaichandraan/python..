268cf9577a997fae3811d5ade77be44272b0700e
annamalai <121892739+annamalaichandraan@users.noreply.github.com>
2023-06-22 22:49:35 +0530

    Add files via upload

---
+++ list_tuple_dict (1).py
@@ -0,0 +1,284 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[12]:
+
+
+friuts="apple","orange"
+friuts
+
+
+# In[9]:
+
+
+_fruitslist="apple"
+
+
+# In[18]:
+
+
+x=["annamalai","hiii","jjj",333,2.7]
+x
+
+
+# In[20]:
+
+
+type(x)
+
+
+# In[21]:
+
+
+x[0]
+
+
+# In[22]:
+
+
+x[0]=45
+
+
+# In[23]:
+
+
+x[3]=45
+
+
+# In[24]:
+
+
+x[3]
+
+
+# In[25]:
+
+
+x
+
+
+# In[26]:
+
+
+x.reverse()
+
+
+# In[27]:
+
+
+x
+
+
+# In[29]:
+
+
+sample_tuple=(45,67,"hii")
+sample_tuple
+
+
+# In[30]:
+
+
+type(sample_tuple)
+
+
+# In[31]:
+
+
+sample_tuple.reverse()
+
+
+# In[32]:
+
+
+smaple_set={1,2,3,4,5}
+
+
+# In[33]:
+
+
+sample_set
+
+
+# In[34]:
+
+
+smaple_set
+
+
+# In[35]:
+
+
+smaple_set.add(6)
+
+
+# In[36]:
+
+
+smaple_set
+
+
+# In[41]:
+
+
+sample_dict={"a":"annamalai",2:"hlooo"}
+sample_dict
+
+
+# In[43]:
+
+
+sample_dict[2]
+
+
+# In[44]:
+
+
+sample_list=["asdfgh","asdfghjk","rtyui"]
+sample_list
+
+
+# In[45]:
+
+
+type(sample_list)
+
+
+# In[46]:
+
+
+sample_list.reverse()
+
+
+# In[47]:
+
+
+sample_list
+
+
+# ###listexamples
+
+# In[48]:
+
+
+sample_list=["apple",22,34,4.5,2+3j,"hii"]
+sample_list
+
+
+# In[49]:
+
+
+sample_list.reverse()
+sample_list
+
+
+# In[50]:
+
+
+sample_list(0)
+
+
+# In[51]:
+
+
+sample_list[0]
+
+
+# In[52]:
+
+
+sample_list[1]="tttt"
+sample_list
+
+
+# ###tuple examples
+
+# ### tuple examples
+
+# In[57]:
+
+
+sample_tuple=(12,22,"guchi")
+sample_tuple
+
+
+# ### while calling the index paranthesis are very important
+
+# In[58]:
+
+
+sample_tuple(1)
+
+
+# In[59]:
+
+
+sample_tuple[1]
+
+
+# In[60]:
+
+
+sample_tuple.reverse()
+
+
+# ### dictionay
+
+# In[62]:
+
+
+sample_dict={1:"annamalai",2:"hlooo"}
+type(sample_dict)
+
+
+# In[63]:
+
+
+sample_dict
+
+
+# In[64]:
+
+
+sample_dict(1)
+
+
+# In[65]:
+
+
+sample_dict[1]
+
+
+# In[66]:
+
+
+sample_dict[1]="hyyyy"
+sample_dict
+
+
+# ### set
+
+# In[68]:
+
+
+sample_set={1,2,3,3,3,3,3,5,6,6,7,8,8,9,0}
+sample_set
+
+
+# In[72]:
+
+
+sample_set.add(4)
+
+
+# In[73]:
+
+
+sample_set.add(3)
+
+
+# In[ ]:
+
+
+
+
