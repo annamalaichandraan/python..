9f5a8b59774bc73f16ab7e2012fb94cdb6ad9d80
annamalai <121892739+annamalaichandraan@users.noreply.github.com>
2023-07-10 10:10:17 +0530

    Add files via upload

---
+++ dictionery.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+printf("ho")
+
+
+# In[2]:
+
+
+print("hello")
+
+
+# In[3]:
+
+
+sample_dict={1:"annamalai",2:4556,3:"enenuuu",3:34.78}
+
+
+# In[4]:
+
+
+sample_dict
+
+
+# In[6]:
+
+
+###has key_value pair data structure
+###key is unique
+###value can be dulpicated
+###value can be retrved using key
+
+
+### these are all peoperties of dictioneries
+
+
+# In[7]:
+
+
+sample_dict(1)
+
+
+# In[8]:
+
+
+sample_dict[1]
+
+
+# In[9]:
+
+
+sample_dict[3]
+
+
+# In[10]:
+
+
+sample_dict[2]=78.45
+
+
+# In[ ]:
+
+
+
+
+
+# In[11]:
+
+
+sample_dict
+
+
+# In[12]:
+
+
+sample_dict[5]=45
+
+
+# In[13]:
+
+
+sample_dict
+
+
+# In[14]:
+
+
+### we can add a key value pair
+
+
+# ## dictionary complted
+
+# ### numpy
+
+# 
